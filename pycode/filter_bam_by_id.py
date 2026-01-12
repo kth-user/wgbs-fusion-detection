b8c486509cbf794d0126ba5b7f313d62e5769048
kth-user <growthofhero@gmail.com>
2026-01-13 04:30:45 +0900

    Modified comments

--- pycode/filter_bam_by_id.py
+++ pycode/filter_bam_by_id.py
@@ -6,11 +6,11 @@ id_file = sys.argv[1]
 input_bam = sys.argv[2]
 output_bam = sys.argv[3]
 
-# Read ID 세트 로드
+# Load Read ID set
 with open(id_file, 'r') as f:
     target_ids = set(line.strip() for line in f)
 
-# BAM 파일 필터링
+# Filter BAM file 
 with pysam.AlignmentFile(input_bam, 'rb') as infile:
     with pysam.AlignmentFile(output_bam, 'wb', 
                            template=infile, header=infile.header) as outfile:
